e641a7331924f88ad0b66f9b0615f72057854d79
ItzIcyGaming <73133185+TrulyItzIcyGaming@users.noreply.github.com>
2024-10-25 12:16:56 -0400

    Update index.tsx

--- src/layouts/RootLayout/ThemeProvider/Global/index.tsx
+++ src/layouts/RootLayout/ThemeProvider/Global/index.tsx
@@ -12,8 +12,8 @@ export const Global = () => {
         body {
           margin: 0;
           padding: 0;
-          color: ${theme.colors.white12};
-          background-color: ${theme.colors.white2};
+          color: ${theme.colors.gray12};
+          background-color: ${theme.colors.gray2};
           font-family: ${pretendard.style.fontFamily};
           font-weight: ${pretendard.style.fontWeight};
           font-style: ${pretendard.style.fontStyle};
@@ -71,7 +71,7 @@ export const Global = () => {
           width: 100%;
           border: none;
           margin: 0;
-          border-top: 1px solid ${theme.colors.white6};
+          border-top: 1px solid ${theme.colors.gray6};
         }
       `}
     />
